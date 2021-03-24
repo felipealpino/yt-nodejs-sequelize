e29ff641f7b57f33c6eb972197432f9e1d29a5bc
Felipe Alpino <felipealpino1@gmail.com>
2021-03-24 15:58:58 -0300

    Requisicoes post com express e postman

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const home = require('./router/home')
 const clients = require('./router/client')
+const bodyParser = require('body-parser')
 
-//Configurações de endpoints
+
+//ROTAS -- Configurações de endpoints
 const app = express();
+
+app.use(express.json()) //body-parser (old library)
+app.use(express.urlencoded({extended:true}))
+
 app.use('/', home)
 app.use('/client', clients)
 
--- router/home.js
+++ router/home.js
@@ -6,5 +6,11 @@ router.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+
+//ROTA PARA TESTAR NO POSTMAN -- http://localhost:7777/body
+router.post('/body', (req, res) => {
+    res.json(req.body)
+})
+
 module.exports = router
 
