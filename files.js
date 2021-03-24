1c2402747e921118c9aa224b83e71b46b02f27f0
Felipe Alpino <felipealpino1@gmail.com>
2021-03-24 14:50:36 -0300

    lendo um arquivo usando fs

--- files.js
+++ files.js
@@ -2,8 +2,16 @@ const fs = require('fs')
 
 // => file system
 
-fs.writeFile ('myFile.txt', 'Mensagem qualquer', (err) => {
-    if(err){
-        console.log("Error: "+err.message)
+// fs.writeFile ('myFile.txt', 'Mensagem qualquer', (err) => {
+//     if(err){
+//         console.log("Error: "+err.message)
+//     }
+// })
+
+fs.readFile('myFile.txt', (err, data) => { 
+    try{
+        console.log(data.toString('utf-8'))
+    } catch(error) {
+        console.log("Ocorreu algum error não esperado")
     }
 })
