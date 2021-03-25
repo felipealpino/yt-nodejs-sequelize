2564d397348ccc93caf75c1513f00c1fc2b2ad67
Felipe Alpino <felipealpino1@gmail.com>
2021-03-24 23:15:56 -0300

    Listando clientes em uma table (select - findAll())

--- router/client.js
+++ router/client.js
@@ -2,26 +2,32 @@ const express = require('express')
 
 const db = require('../models')
 const {Client} = require('../models')
-
 const router = express.Router();
+
 //onde '/' é do prefix /clients
 router.get('/', (req, res) => {
     res.send('Client page')
 })
+
 router.get('/new', (req, res) => {
     res.render('formClient')
 })
-router.post('/new', (req, res) => {
+
+router.get('/list', async (req,res) => {
+    const dados = await Client.findAll()
+    res.render('listClients',{
+        dados:dados
+    })
+})
+
+router.post('/new', async (req, res) => {
     console.log(req.body)
-    const dados = Client.create(req.body)
+    const dados = await Client.create(req.body)
     console.log(dados)
     console.log('User successfully added !!')
     res.redirect('/')
 })
 
-router.get('/welcome', (req, res) => {
-    res.send('Client page Welcome')
-})
 
 
 module.exports = router
