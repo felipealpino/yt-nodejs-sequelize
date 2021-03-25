3a22007eab6dd790ee98e5450df2533ef4ec3882
Felipe Alpino <felipealpino1@gmail.com>
2021-03-25 00:06:08 -0300

    Delete feito e ainda apanhando para o edit

--- router/client.js
+++ router/client.js
@@ -9,24 +9,45 @@ router.get('/', (req, res) => {
     res.send('Client page')
 })
 
+
 router.get('/new', (req, res) => {
     res.render('formClient')
 })
-
-router.get('/list', async (req,res) => {
-    const dados = await Client.findAll()
-    res.render('listClients',{
-        dados:dados
-    })
-})
-
 router.post('/new', async (req, res) => {
     console.log(req.body)
     const dados = await Client.create(req.body)
-    console.log(dados)
+    // console.log(dados)
     console.log('User successfully added !!')
     res.redirect('/')
 })
+
+
+router.get('/list', async (req,res) => {
+    const dados = await Client.findAll()
+    res.render('listClients', {dados:dados})
+})
+
+
+router.get('/edit/:id', async (req, res) => {
+    const dados = await Client.findAll({
+        where: {id:req.params.id}
+    });
+    res.render('editFormClient', {dados:dados})
+})
+
+
+router.get('/delete/:id', async (req, res) => {
+    await Client.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    console.log("Cliente id: "+req.params.id+" was deleted")
+    res.redirect('/client/list')
+})
+
+
+
 
 
 
